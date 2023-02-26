8b69bcc2d7072316367ef05d4722a3b1bf2b272c
jamescross813 <crossjames813@gmail.com>
2023-02-25 23:09:38 -0600

    set up get_traveler_location() func

--- script.py
+++ script.py
@@ -1,7 +1,8 @@
-destinations = ["Paris, France", "Shanghai, China", "Los Angeles, USA", "São Paulo, Brazil", "Cairo, Egypt"]
+destinations = ["Paris, France", "Shanghai, China", "Los Angeles, USA", "Sao Paulo, Brazil", "Cairo, Egypt"]
 test_traveler = ["Erin Wilkes", "Shanghai, China", ["historical site", "art"]]
 
 def get_destination_index(destination):
     return destinations.index(destination)
 
 
+print(get_destination_index("Los Angeles, USA"))
