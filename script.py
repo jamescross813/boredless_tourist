4b144e184106a3685d514499544c93de1a873ddd
jamescross813 <crossjames813@gmail.com>
2023-02-25 23:10:51 -0600

    get_traveler_location()function written

--- script.py
+++ script.py
@@ -4,5 +4,7 @@ test_traveler = ["Erin Wilkes", "Shanghai, China", ["historical site", "art"]]
 def get_destination_index(destination):
     return destinations.index(destination)
 
+def get_traveler_location(traveler):
+    return traveler[1]
 
 print(get_destination_index("Los Angeles, USA"))
