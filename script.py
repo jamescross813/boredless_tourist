2fb952bcfb4eaeb141d8d2fd472bf7b6020de3f9
jamescross813 <crossjames813@gmail.com>
2023-02-25 23:02:52 -0600

    fixed typo

--- script.py
+++ script.py
@@ -1,2 +1,2 @@
 destinations = ["Paris, France", "Shanghai, China", "Los Angeles, USA", "São Paulo, Brazil", "Cairo, Egypt"]
-test_traveller = ["Erin Wilkes", "Shanghai, China", ["historical site", "art"]]
+test_traveler = ["Erin Wilkes", "Shanghai, China", ["historical site", "art"]]
