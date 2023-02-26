4e319ed26b3708c823f0c048024a9c241a350b55
jamescross813 <crossjames813@gmail.com>
2023-02-25 23:01:24 -0600

    test traveller var added

--- script.py
+++ script.py
@@ -1,1 +1,2 @@
 destinations = ["Paris, France", "Shanghai, China", "Los Angeles, USA", "São Paulo, Brazil", "Cairo, Egypt"]
+test_traveller = ["Erin Wilkes", "Shanghai, China", ["historical site", "art"]]
