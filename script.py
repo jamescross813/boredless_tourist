8782148aba14728b5787efefeae88f55db9aa3d1
jamescross813 <crossjames813@gmail.com>
2023-02-26 19:08:40 -0600

    atempt to make empty list

--- script.py
+++ script.py
@@ -1,6 +1,6 @@
 destinations = ["Paris, France", "Shanghai, China", "Los Angeles, USA", "Sao Paulo, Brazil", "Cairo, Egypt"]
 test_traveler = ["Erin Wilkes", "Shanghai, China", ["historical site", "art"]]
-attractions = [[], [], [], [], []]
+attractions = []
 
 def get_destination_index(destination):
     return destinations.index(destination)
@@ -9,7 +9,8 @@ def get_traveler_location(traveler):
     return traveler[1]
 
 def get_attractions():
-
+    for dest in destinations:
+        attractions.append([])
 
 # print(get_destination_index("Los Angeles, USA"))
 # print(get_traveler_location(test_traveler))
