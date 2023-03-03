6f981bfc33cd90425891b424046d1a332398fc24
jamescross813 <crossjames813@gmail.com>
2023-03-02 21:08:19 -0600

    bug tracking

--- script.py
+++ script.py
@@ -21,24 +21,6 @@ def add_attraction(destination, attraction):
         attractions_for_destination.append(attraction)
     return attractions_for_destination
 
-def find_attractions(destination, interests):
-    destination_index = get_destination_index(destination)
-    attractions_in_city = attractions[destination_index]
-    attractions_with_interest =[]
-    for attrac in attractions_in_city:
-        attraction_tag=attrac[1]
-        for int in interests:
-            if int == attraction_tag:
-                attractions_with_interest.append(attrac)
-    return attractions_with_interest
-
-# print(get_destination_index("Los Angeles, USA"))
-# print(get_traveler_location(test_traveler))
-# test_destination_index = get_destination_index(get_traveler_location(test_traveler))
-# print(test_destination_index)
-# print(attractions)
-
-# additional attractions
 add_attraction("Los Angeles, USA", ['Venice Beach', ['beach']])
 add_attraction("Paris, France", ["the Louvre", ["art", "museum"]])
 add_attraction("Paris, France", ["Arc de Triomphe", ["historical site", "monument"]])
@@ -51,4 +33,26 @@ add_attraction("Sao Paulo, Brazil", ["Patio do Colegio", ["historical site"]])
 add_attraction("Cairo, Egypt", ["Pyramids of Giza", ["monument", "historical site"]])
 add_attraction("Cairo, Egypt", ["Egyptian Museum", ["museum"]])
 
-print(attractions)
+def find_attractions(destination, interests):
+    destination_index = get_destination_index(destination)
+    attractions_in_city = attractions[destination_index]
+    attractions_with_interest =[]
+    for attrac in attractions_in_city:
+        attraction_tag=attrac[1]
+        for int in interests:
+            print(attraction_tag)
+            if int == attraction_tag:
+                attractions_with_interest.append(attrac)
+    return attractions_with_interest
+
+print(find_attractions("Los Angeles, USA", ['art']))
+# print(get_destination_index("Los Angeles, USA"))
+# print(get_traveler_location(test_traveler))
+# test_destination_index = get_destination_index(get_traveler_location(test_traveler))
+# print(test_destination_index)
+# print(attractions)
+
+# additional attractions
+
+
+# print(attractions)
